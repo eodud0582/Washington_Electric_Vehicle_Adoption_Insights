64328781779140d017cc095b3e6d6b013ca7376d
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-10-18 23:25:00 -0400

    Update ev_analysis_streamlit.py

--- ev_analysis_streamlit.py
+++ ev_analysis_streamlit.py
@@ -70,7 +70,7 @@ st.title("Electric Vehicle Adoption in Washington State: Trends, Economy, Infras
 
 col1, col2, col3 = st.columns([1,6,1])
 with col1: st.write("")
-with col2: st.image("https://github.com/user-attachments/assets/1423a53e-14c0-427f-8dd1-b3ad207607e7")
+with col2: st.image("https://github.com/user-attachments/assets/a599e7a7-8468-4ba7-808b-95e71a799a87")
 with col3: st.write("")
 
 st.markdown("""
