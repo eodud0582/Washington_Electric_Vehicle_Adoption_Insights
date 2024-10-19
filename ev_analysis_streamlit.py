14ad28b012d5843b8a11fcf6881cfb9cd1bf3199
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-10-19 03:17:19 -0400

    Update ev_analysis_streamlit.py

--- ev_analysis_streamlit.py
+++ ev_analysis_streamlit.py
@@ -104,7 +104,14 @@ st.divider()
 # with col3: st.write("")
 
 st.warning("This dashboard explores the relationship between electric vehicle")
-st.info("This dashboard explores the relationship between electric vehicle")
+st.info("""
+This dashboard explores the relationship between electric vehicle (EV) adoption in Washington State, economic factors, infrastructure developments, and political trends. The goal is to provide insights for policymakers, industry stakeholders, and researchers interested in promoting sustainable transportation.
+
+**Designed for:**
+- Washington State government policymakers
+- Electric vehicle manufacturers and related industry professionals
+- Policymakers and researchers from other states
+""")
 st.success("This dashboard explores the relationship between electric vehicle")
 st.error("This dashboard explores the relationship between electric vehicle")
 st.markdown(
