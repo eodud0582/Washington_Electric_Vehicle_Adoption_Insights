a8b0c4d5b89ca2831ff957b57e696d5981a1ba4b
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-10-19 03:09:36 -0400

    Update ev_analysis_streamlit.py

--- ev_analysis_streamlit.py
+++ ev_analysis_streamlit.py
@@ -893,7 +893,7 @@ st.sidebar.markdown("""
 
 st.sidebar.header("Table of Contents")
 st.sidebar.markdown("""
-- [Home](#electric-vehicle-adoption-in-washington-state-trends-economy-infrastructure-and-politics)
+- [Home](#electric-vehicle-adoption-in-washington-state)
 - [1. Overview of EV Adoption in Washington](#1-overview-of-ev-adoption-in-washington)
 - [2. EV Adoption Trends](#2-ev-adoption-trends)
 - [3. Economic Indicator](#3-economic-indicator)
