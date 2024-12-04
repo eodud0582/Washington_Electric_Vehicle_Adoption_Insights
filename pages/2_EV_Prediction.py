ad223932210f01bac972cb65b272da11a4875306
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-12-04 17:51:07 -0500

    Update 2_EV_Prediction.py

--- pages/2_EV_Prediction.py
+++ pages/2_EV_Prediction.py
@@ -176,7 +176,7 @@ with col2:
         tooltip=['Feature', 'Importance']
     ).properties(
         width='container', # Adaptive width
-        height=170 # Height for clarity
+        height='container' # 170 # Height for clarity
     )
     
     st.altair_chart(chart, use_container_width=True)
