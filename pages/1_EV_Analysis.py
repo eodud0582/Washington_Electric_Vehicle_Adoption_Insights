da5eceaad9c5af1719ae50f2807d540d40d9e75b
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-12-04 18:11:32 -0500

    Update 1_EV_Analysis.py

--- pages/1_EV_Analysis.py
+++ pages/1_EV_Analysis.py
@@ -21,9 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import pickle
+
 import pandas as pd
 import numpy as np
+import pickle
 import statsmodels.api as sm
 
 import streamlit as st
