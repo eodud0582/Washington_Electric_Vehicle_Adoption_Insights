ae36f50983559014f502afcc788978497c2f9984
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-12-06 01:50:23 -0500

    Update 2_EV_Prediction.py

--- pages/2_EV_Prediction.py
+++ pages/2_EV_Prediction.py
@@ -273,15 +273,14 @@ with col2:
         shap_values.values[0],
         feature_names=selected_features,
         matplotlib=False, # Render as HTML
-        plot_cmap=[red_color, highlight_color],
-        show=False
+        plot_cmap=[red_color, highlight_color]
     )
     # Embed/wrap the SHAP force plot in Streamlit using an CSS/iframe
     # - Rendering shap.force_plot as an HTML plot in Streamlit requires wrapping it in an iframe. 
     # - Streamlit doesn't natively support direct HTML rendering for SHAP visualizations.
     # - Need to save the force plot as an interactive HTML snippet and embed it using st.components.v1.html.
     shap_html = f"<head>{shap.getjs()}</head><body>{force_plot_html.html()}</body>"
-    html(shap_html, height=140)
+    html(shap_html, height=160, width='container')
     
     # fig = shap.force_plot(
     #     explainer.expected_value,
