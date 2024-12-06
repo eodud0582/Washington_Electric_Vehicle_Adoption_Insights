31e9b066417eccc15580e77b1d48cce3661ebe78
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-12-06 01:56:44 -0500

    Update 2_EV_Prediction.py

--- pages/2_EV_Prediction.py
+++ pages/2_EV_Prediction.py
@@ -268,42 +268,19 @@ with col1:
 with col2:
     # Generate SHAP force plot (interactive visualization)
     st.write("### Variable Impact Direction (SHAP Force Plot)")
-    # force_plot_html = shap.force_plot(
-    #     explainer.expected_value,
-    #     shap_values.values[0],
-    #     feature_names=selected_features,
-    #     matplotlib=False, # Render as HTML
-    #     plot_cmap=[red_color, highlight_color]
-    # )
-    # # Embed/wrap the SHAP force plot in Streamlit using an CSS/iframe
-    # # - Rendering shap.force_plot as an HTML plot in Streamlit requires wrapping it in an iframe. 
-    # # - Streamlit doesn't natively support direct HTML rendering for SHAP visualizations.
-    # # - Need to save the force plot as an interactive HTML snippet and embed it using st.components.v1.html.
-    # shap_html = f"<head>{shap.getjs()}</head><body>{force_plot_html.html()}</body>"
-    # html(shap_html, height=160)
-
-    # Generate SHAP force plot (interactive visualization)
     force_plot_html = shap.force_plot(
         explainer.expected_value,
         shap_values.values[0],
         feature_names=selected_features,
-        matplotlib=False,  # Render as HTML
+        matplotlib=False, # Render as HTML
         plot_cmap=[red_color, highlight_color]
     )
+    # Embed/wrap the SHAP force plot in Streamlit using an CSS/iframe
+    # - Rendering shap.force_plot as an HTML plot in Streamlit requires wrapping it in an iframe. 
+    # - Streamlit doesn't natively support direct HTML rendering for SHAP visualizations.
+    # - Need to save the force plot as an interactive HTML snippet and embed it using st.components.v1.html.
     shap_html = f"<head>{shap.getjs()}</head><body>{force_plot_html.html()}</body>"
-
-    # Create a custom CSS style to make the iframe height adaptive
-    css_style = """
-    <style>
-    #shap-force-plot {
-        width: 100%;
-        height: auto; /* Set height to auto to fill the container */
-    }
-    </style>
-    """
-    
-    # Embed the SHAP force plot with the custom CSS
-    st.components.v1.html(css_style + shap_html)
+    html(shap_html, height=160)
 
     # fig = shap.force_plot(
     #     explainer.expected_value,
