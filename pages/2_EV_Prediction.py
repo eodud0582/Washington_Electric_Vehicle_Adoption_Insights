ea349a79c691d4c5d5227bec6b6f059d7a3d6dc7
Daeyoung Kim <38115693+eodud0582@users.noreply.github.com>
2024-12-06 01:34:34 -0500

    Update 2_EV_Prediction.py

--- pages/2_EV_Prediction.py
+++ pages/2_EV_Prediction.py
@@ -280,11 +280,7 @@ with col2:
     # - Streamlit doesn't natively support direct HTML rendering for SHAP visualizations.
     # - Need to save the force plot as an interactive HTML snippet and embed it using st.components.v1.html.
     shap_html = f"<head>{shap.getjs()}</head><body>{force_plot_html.html()}</body>"
-    # html(shap_html, height=140, width=700)
-    html(
-        f'<div style="width:100%; margin:auto;">{shap_html}</div>', 
-        height=160
-    )
+    html(shap_html, height=140, width=700)
     
     # fig = shap.force_plot(
     #     explainer.expected_value,
